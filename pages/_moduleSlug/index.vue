9eddfcda8ce730cd76add8c736c156bd6a3c1744
chihaoyo <chihaoyo@gmail.com>
2018-04-19 10:39:29 +0800

    Add module page

---
+++ pages/_moduleSlug/index.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+import { projects, modules } from '~/config'
+
+export default {
+  async asyncData({ params }) {
+    let projectsOfModule = projects.filter(project => project.module === params.moduleSlug)
+    let module = modules.find(module => module.id === params.moduleSlug)
+
+    return {
+      projects: projectsOfModule,
+      module
+    }
+  }
+}
+</script>
