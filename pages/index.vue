b9faa01805802d2c376612a519197afe6d995895
chihaoyo <chihaoyo@gmail.com>
2018-04-25 12:52:01 +0800

    Update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,22 @@
 <template>
 <div class="page index">
-  <no-ssr><welcome /></no-ssr>
-  <nuxt-link class="project a-block" v-for="project of projects" :key="project.id" :to="{ name: 'moduleSlug-projectSlug', params: { moduleSlug: project.module, projectSlug: project.id }}"><span class="a-target">{{ project.title }}</span></nuxt-link>
+  <div class="tcl-container">
+    <div class="tcl-panel full-width with-padding">
+      <no-ssr><welcome /></no-ssr>
+    </div>
+  </div>
+  <div class="tcl-container">
+    <div class="tcl-panel" v-for="project of projects">
+      <nuxt-link class="project a-block" :key="project.id" :to="{ name: 'moduleSlug-projectSlug', params: { moduleSlug: project.module, projectSlug: project.id }}"><span class="a-target">{{ project.title }}</span></nuxt-link>
+    </div>
+    <div class="tcl-panel"></div>
+  </div>
 </div>
 </template>
 
 <script>
 import { projects, modules } from '~/config'
-import Welcome from '~/components/Welcome'
+import Welcome from 'watchout-common-functions/components/Welcome'
 
 export default {
   data() {
