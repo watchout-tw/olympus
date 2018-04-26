ea50f1adc1ff1b4b299573497123291c77cc96d8
chihaoyo <chihaoyo@gmail.com>
2018-04-26 17:39:18 +0800

    Use with-left-right-margin

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="error">
-  <div class="content center-self center-child">
+  <div class="content center-self with-left-right-margin center-child">
     <h1 class="large">{{ error.statusCode }}</h1>
     <p>在什麼都有的肥沃草原裡，也是有遍尋不著的東西。</p>
     <nuxt-link to="/" class="a-text">找回初衷</nuxt-link>
