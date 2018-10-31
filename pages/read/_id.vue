d7bc5106fa351f119e216f7f27b86a94f9fad0b3
chihaoyo <chihaoyo@gmail.com>
2018-11-01 00:40:30 +0800

    Add /read

---
+++ pages/read/_id.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="page read">
+  <article>This is an article to read.</article>
+</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
