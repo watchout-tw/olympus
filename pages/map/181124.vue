af2c1820384b21ddee637b551249fb98497e851f
chihaoyo <chihaoyo@gmail.com>
2018-11-26 18:58:49 +0800

    Add 181124 page & data

---
+++ pages/map/181124.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="page">
+  <map-box :markers="reports" />
+</div>
+</template>
+
+<script>
+import reports from '~/data/map/2018-election-international-coverage.json'
+import MapBox from '~/components/MapBox'
+
+export default {
+  data() {
+    let mapID = 'map-' + new Date().getTime()
+    return {
+      mapID,
+      reports
+    }
+  },
+  components: {
+    MapBox
+  }
+}
+</script>
