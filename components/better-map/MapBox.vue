d076280b9bab207e9b3d85a7ca99afc74ea2198d
chihaoyo <chihaoyo@gmail.com>
2018-12-12 18:10:24 +0800

    Add playback functionality to MapBox

--- components/better-map/MapBox.vue
+++ components/better-map/MapBox.vue
@@ -2,6 +2,11 @@
 <div class="map-box">
   <div class="map-container">
     <div class="map content" id="map"></div>
+    <div class="datetime">{{ currentDateTime }}</div>
+  </div>
+  <div v-if="config && config.live" class="controls form-field-many-inputs justify-center margin-top-bottom-8">
+    <div class="input button large musou" @click="togglePlay"><span>{{ isPlaying ? '暫停' : '播放' }}</span></div>
+    <div class="input button" @click="quitPlay" v-if="nextToPlay > -1"><span>跳出</span></div>
   </div>
   <div class="note secondary-text font-size-small margin-top-4 d-flex align-items-center justify-content-center"><span>提示：點擊地圖上的圖示</span><span style="display: inline-block; font-size: 1.5rem; margin: 0 0.125rem;">④</span><span>看當地新聞</span></div>
   <div class="markers tcl-container">
@@ -22,6 +27,27 @@
 <script>
 import config from 'watchout-common-functions/config/config'
 
+const SRC_STATIC = 'markers'
+const SRC_LIVE = 'markers-live'
+const SOURCES = [ SRC_STATIC, SRC_LIVE ]
+
+const LAYER_CL = 'clusters'
+const LAYER_CL_COUNT = 'cluster-count'
+const LAYER_MARKERS = 'markers-not-in-cluster'
+const LAYER_LIVE = 'markers-live'
+const LAYERS = [ LAYER_CL, LAYER_CL_COUNT, LAYER_MARKERS, LAYER_LIVE ]
+
+function makeFeature(marker) {
+  return {
+    type: 'Feature',
+    properties: marker,
+    geometry: {
+      type: 'Point',
+      coordinates: [marker.lng, marker.lat]
+    }
+  }
+}
+
 export default {
   props: ['markers', 'config'],
   data() {
@@ -29,50 +55,108 @@ export default {
       map: null,
       mapElementID: 'map',
       selectedClusterID: null,
-      selectedMarkers: []
+      selectedMarkers: [],
+      staticDS: null,
+      liveDS: null,
+      isPlaying: false,
+      nextToPlay: -1,
+      timer: null
+    }
+  },
+  computed: {
+    nextMarker() {
+      return this.nextToPlay >= 0 && this.nextToPlay < this.markers.length ? this.markers[this.nextToPlay] : null
+    },
+    currentDateTime() {
+      return this.nextToPlay >= 0 && this.nextToPlay < this.markers.length ? this.nextMarker.date : this.markers[this.markers.length - 1].date
     }
   },
   mounted() {
-    const mapbox = require('mapbox-gl')
-    // const MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder')
-    mapbox.accessToken = config.mapboxAccessToken
-
-    this.map = new mapbox.Map({
-      container: this.mapElementID,
-      style: 'mapbox://styles/watchout/cjozx93ng11m72rlqumr7uobd',
-      center: this.config && this.config.center ? this.config.center : { lat: 23.9609981, lng: 120.9718638 },
-      zoom: this.config && this.config.zoom ? this.config.zoom : 1
-    })
-    this.map.addControl(new mapbox.NavigationControl(), 'top-left')
-    // this.map.addControl(new MapboxGeocoder({ accessToken: config.mapboxAccessToken }), 'top-left')
-    let geojson = {
-      type: 'FeatureCollection',
-      features: this.markers.map(marker => {
-        return {
-          type: 'Feature',
-          properties: marker,
-          geometry: {
-            type: 'Point',
-            coordinates: [marker.lng, marker.lat]
-          }
+    this.init()
+  },
+  methods: {
+    clear() {
+      LAYERS.forEach(layerName => {
+        if(this.map.getLayer(layerName)) {
+          this.map.removeLayer(layerName)
         }
       })
-    }
-    // https://www.mapbox.com/mapbox-gl-js/example/add-image/
-    // https://www.mapbox.com/mapbox-gl-js/example/popup-on-click/
-    // https://www.mapbox.com/mapbox-gl-js/example/cluster/
-    const sourceID = 'markers'
-    let clusterRadius = 50
-    this.map.on('load', (e) => {
-      this.map.addSource(sourceID, {
-        type: 'geojson',
-        data: geojson,
-        cluster: true,
-        clusterMaxZoom: 14,
-        clusterRadius
+      SOURCES.forEach(sourceName => {
+        if(this.map.getSource(sourceName)) {
+          this.map.removeSource(sourceName)
+        }
       })
+      this.selectedClusterID = null
+      this.selectedMarkers = []
+      this.isPlaying = false
+      this.nextToPlay = -1
+      window.clearInterval(this.timer)
+      this.timer = null
+    },
+    init() {
+      const mapbox = require('mapbox-gl')
+      // const MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder')
+      mapbox.accessToken = config.mapboxAccessToken
+
+      this.map = new mapbox.Map({
+        container: this.mapElementID,
+        style: 'mapbox://styles/watchout/cjozx93ng11m72rlqumr7uobd',
+        center: this.config && this.config.center ? this.config.center : { lat: 23.9609981, lng: 120.9718638 },
+        zoom: this.config && this.config.zoom ? this.config.zoom : 1
+      })
+      this.map.addControl(new mapbox.NavigationControl(), 'top-left')
+      // this.map.addControl(new MapboxGeocoder({ accessToken: config.mapboxAccessToken }), 'top-left')
+
+      // https://www.mapbox.com/mapbox-gl-js/example/add-image/
+      // https://www.mapbox.com/mapbox-gl-js/example/popup-on-click/
+      // https://www.mapbox.com/mapbox-gl-js/example/cluster/
+      this.map.on('load', this.draw)
+      this.map.on('click', 'clusters', (e) => {
+        let cluster = this.map.queryRenderedFeatures(e.point, { layers: ['clusters'] })[0]
+        let clusterCoordinates = cluster.geometry.coordinates
+
+        // https://stackoverflow.com/a/52344700
+        // https://www.mapbox.com/mapbox-gl-js/api/#geojsonsource#getclusterleaves
+        let clusterID = cluster.properties.cluster_id
+        let markerCount = cluster.properties.point_count
+        let source = this.map.getSource(SRC_STATIC)
+        source.getClusterLeaves(clusterID, markerCount, 0, (error, markers) => {
+          if(error) {
+            console.error(error)
+          }
+          this.selectedMarkers = markers
+          this.map.flyTo({
+            center: clusterCoordinates,
+            zoom: this.map.getZoom() + 1
+          })
+        })
+      })
+      this.map.on('click', 'markers-not-in-cluster', (e) => {
+        let marker = e.features[0]
+        let coordinates = marker.geometry.coordinates.slice()
+        this.selectedMarkers = [marker]
+        this.map.flyTo({
+          center: coordinates,
+          zoom: this.map.getZoom() + 1
+        })
+      })
+    },
+    draw() {
+      if(!this.map.getSource(SRC_STATIC)) {
+        this.staticDS = {
+          type: 'FeatureCollection',
+          features: this.markers.map(makeFeature)
+        }
+        this.map.addSource(SRC_STATIC, {
+          type: 'geojson',
+          data: this.staticDS,
+          cluster: true,
+          clusterMaxZoom: 14,
+          clusterRadius: 50
+        })
+      }
       this.map.addLayer({
-        id: 'clusters',
+        id: LAYER_CL,
         type: 'circle',
         source: 'markers',
         filter: ['has', 'point_count'],
@@ -98,7 +182,7 @@ export default {
         }
       })
       this.map.addLayer({
-        id: 'cluster-count',
+        id: LAYER_CL_COUNT,
         type: 'symbol',
         source: 'markers',
         filter: ['has', 'point_count'],
@@ -109,7 +193,7 @@ export default {
         }
       })
       this.map.addLayer({
-        id: 'markers-not-in-cluster',
+        id: LAYER_MARKERS,
         type: 'circle',
         source: 'markers',
         filter: ['!', ['has', 'point_count']],
@@ -118,36 +202,53 @@ export default {
           'circle-radius': 8
         }
       })
-    })
-    this.map.on('click', 'clusters', (e) => {
-      let cluster = this.map.queryRenderedFeatures(e.point, { layers: ['clusters'] })[0]
-      let clusterCoordinates = cluster.geometry.coordinates
-
-      // https://stackoverflow.com/a/52344700
-      // https://www.mapbox.com/mapbox-gl-js/api/#geojsonsource#getclusterleaves
-      let clusterID = cluster.properties.cluster_id
-      let markerCount = cluster.properties.point_count
-      let source = this.map.getSource(sourceID)
-      source.getClusterLeaves(clusterID, markerCount, 0, (error, markers) => {
-        if(error) {
-          console.error(error)
+    },
+    preparePlay() {
+      this.clear()
+      if(!this.map.getSource(SRC_LIVE)) {
+        this.liveDS = {
+          type: 'FeatureCollection',
+          features: []
         }
-        this.selectedMarkers = markers
-        this.map.flyTo({
-          center: clusterCoordinates,
-          zoom: this.map.getZoom() + 1
+        this.map.addSource(SRC_LIVE, {
+          type: 'geojson',
+          data: this.liveDS
         })
+      }
+      this.map.addLayer({
+        id: LAYER_LIVE,
+        type: 'circle',
+        source: SRC_LIVE,
+        paint: this.config.liveLayerPaint
       })
-    })
-    this.map.on('click', 'markers-not-in-cluster', (e) => {
-      let marker = e.features[0]
-      let coordinates = marker.geometry.coordinates.slice()
-      this.selectedMarkers = [marker]
-      this.map.flyTo({
-        center: coordinates,
-        zoom: this.map.getZoom() + 1
-      })
-    })
+      this.nextToPlay = 0
+    },
+    togglePlay() {
+      if(this.isPlaying) {
+        // pause
+        window.clearInterval(this.timer)
+        this.isPlaying = false
+      } else {
+        if(this.nextToPlay < 0 || this.nextToPlay >= this.markers.length) {
+          this.preparePlay()
+        }
+        // resume
+        this.isPlaying = true
+        this.timer = window.setInterval(() => {
+          if(this.nextMarker) {
+            this.liveDS.features.push(makeFeature(this.nextMarker))
+            this.map.getSource(SRC_LIVE).setData(this.liveDS)
+            this.nextToPlay += 1
+          } else {
+            this.togglePlay()
+          }
+        }, 100)
+      }
+    },
+    quitPlay() {
+      this.clear()
+      this.draw()
+    }
   }
 }
 </script>
@@ -161,6 +262,7 @@ export default {
 .map-box {
   width: 100%;
   > .map-container {
+    position: relative;
     display: flex;
     width: 100%;
     max-width: $tcl-bp-lg;
@@ -196,6 +298,15 @@ export default {
         line-height: 1;
         padding:0 0.25rem;
       }
+    }
+    > .datetime {
+      position: absolute;
+      top: 0;
+      right: 0;
+      padding: 0.25rem 0.5rem;
+      background: white;
+      font-size: $font-size-small;
+      font-weight: bold;
     }
   }
   > .markers {
