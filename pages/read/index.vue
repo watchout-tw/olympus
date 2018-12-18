95524c636893355d421dd7c2914bb90516acbc69
chihaoyo <chihaoyo@gmail.com>
2018-12-18 14:36:49 +0800

    Add image to doc list

--- pages/read/index.vue
+++ pages/read/index.vue
@@ -2,7 +2,7 @@
 <div class="page read index">
   <div class="docs tcl-container margin-top-bottom-8">
     <nuxt-link :to="{ path: doc.id }" append class="doc tcl-panel half-width a-block" v-for="doc of docs" :key="doc.id">
-      <div class="image"></div>
+      <div class="image" :style="getDocImageStyles(doc)"></div>
       <div class="title margin-top-bottom-8"><span class="a-target">{{ doc.title }}</span></div>
     </nuxt-link>
     <div class="tcl-panel half-width"></div>
@@ -13,6 +13,7 @@
 </template>
 
 <script>
+import { env } from 'watchout-common-assets'
 import * as firestore from 'watchout-common-functions/lib/firestore'
 
 export default {
@@ -20,6 +21,17 @@ export default {
     let docs = await firestore.bunko.getDocs()
     return {
       docs
+    }
+  },
+  methods: {
+    getDocImageStyles(doc) {
+      let styles = {}
+      if(doc.image) {
+        if(doc.type === 'ghost') {
+          styles.backgroundImage = `url('${env.links.bunko.beta}${doc.image}')`
+        }
+      }
+      return styles
     }
   }
 }
@@ -34,6 +46,8 @@ export default {
       > .image {
         @include rect(2/1);
         background-color: $color-very-very-light-grey;
+        background-position: center center;
+        background-size: cover;
         @include shadow;
       }
     }
