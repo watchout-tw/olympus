3f6cb24458cfd30630825e7544a2368aa15f3ef7
chihaoyo <chihaoyo@gmail.com>
2018-12-18 18:27:55 +0800

    Add authors

--- pages/read/index.vue
+++ pages/read/index.vue
@@ -1,10 +1,17 @@
 <template>
 <div class="page read index">
   <div class="docs tcl-container margin-top-bottom-8">
-    <nuxt-link :to="{ path: doc.id }" append class="doc tcl-panel half-width a-block" v-for="doc of docs" :key="doc.id">
-      <div class="image" :style="getDocImageStyles(doc)"></div>
-      <div class="title margin-top-bottom-8"><span class="a-target">{{ doc.title }}</span></div>
-    </nuxt-link>
+    <div class="doc tcl-panel half-width" v-for="doc of docs" :key="doc.id">
+      <nuxt-link :to="{ path: doc.id }" append class="anchor a-block">
+        <div class="image" :style="getDocImageStyles(doc)"></div>
+        <div class="title margin-top-bottom-8"><span class="a-target">{{ doc.title }}</span></div>
+      </nuxt-link>
+      <div class="authors font-size-small">
+        <a class="author a-text" :href="getParkPersonaProfileURL(getAuthorByContent(doc.type, author).persona)" target="_blank" v-for="(author, index) of doc.authors" :key="index">
+          {{ getAuthorByContent(doc.type, author).name }}
+        </a>
+      </div>
+    </div>
     <div class="tcl-panel half-width"></div>
     <div class="tcl-panel half-width"></div>
     <div class="tcl-panel half-width"></div>
@@ -14,16 +21,23 @@
 
 <script>
 import { env } from 'watchout-common-assets'
+import { knowsWatchout } from 'watchout-common-functions/interfaces'
 import * as firestore from 'watchout-common-functions/lib/firestore'
 
 export default {
+  mixins: [knowsWatchout],
   async asyncData() {
+    let authors = await firestore.bunko.getAuthors()
     let docs = await firestore.bunko.getDocs()
     return {
+      authors,
       docs
     }
   },
   methods: {
+    getAuthorByContent(type, id) {
+      return this.authors ? this.authors.find(author => author.content.id === id) : null
+    },
     getDocImageStyles(doc) {
       let styles = {}
       if(doc.image) {
@@ -43,12 +57,14 @@ export default {
 .page.read.index {
   > .docs {
     > .doc {
-      > .image {
-        @include rect(2/1);
-        background-color: $color-very-very-light-grey;
-        background-position: center center;
-        background-size: cover;
-        @include shadow;
+      > .anchor {
+        > .image {
+          @include rect(2/1);
+          background-color: $color-very-very-light-grey;
+          background-position: center center;
+          background-size: cover;
+          @include shadow;
+        }
       }
     }
   }
