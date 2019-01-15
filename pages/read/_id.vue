c4b100629b42c9b56317fcf13f5c8a838b627dc8
chihaoyo <chihaoyo@gmail.com>
2019-01-15 16:40:45 +0800

    Use DocHeader

--- pages/read/_id.vue
+++ pages/read/_id.vue
@@ -3,32 +3,19 @@
   <div class="controls responsive-typesetting-container margin-top-bottom-single">
     <nuxt-link :to="{ name: 'read' }" class="a-text">返回列表</nuxt-link>
   </div>
-  <div class="before-article responsive-typesetting-container margin-top-bottom-single">
-    <div class="title variable-font-size margin-top-bottom-single">
-      <h1 class="medium">{{ doc.title }}</h1>
-    </div>
-    <div class="authors">
-      <template v-for="(author, index) of doc.authors">
-        <a class="author a-text" :href="getParkPersonaProfileURL(author)">{{ cachedAuthorByPersona(author).name }}</a>
-        <span v-if="index < doc.authors.length - 1">、</span>
-      </template>
-    </div>
-    <div class="dates font-size-small">
-      <div><span>發佈時間</span><span class="full-width-punctuation">：</span><span>{{ getDateTimeString(doc.publishedAt) }}</span></div>
-      <div v-if="doc.updatedAt"><span>最後更新</span><span class="full-width-punctuation">：</span><span>{{ getDateTimeString(doc.updatedAt) }}</span></div>
-    </div>
-  </div>
+  <doc-header :doc="doc" :cachedAuthors="cachedAuthors" />
   <ghost-article :article="doc.content" :data="referenceData" />
 </div>
 </template>
 
 <script>
 import * as firestore from 'watchout-common-functions/lib/firestore'
-import { knowsCaching, knowsFormatting, knowsWatchout } from 'watchout-common-functions/interfaces'
+import { knowsCaching } from 'watchout-common-functions/interfaces'
 import { getReferenceData } from 'watchout-common-functions/lib/bunko'
+import DocHeader from 'watchout-common-functions/components/comp/DocHeader'
 import GhostArticle from 'watchout-common-functions/components/ghost/Article'
 export default {
-  mixins: [knowsCaching, knowsFormatting, knowsWatchout],
+  mixins: [knowsCaching],
   async asyncData({ params }) {
     let doc = await firestore.bunko.getDoc(params.id, true)
     let referenceData = await getReferenceData('mobiledoc', JSON.parse(doc.content.mobiledoc))
@@ -38,6 +25,7 @@ export default {
     }
   },
   components: {
+    DocHeader,
     GhostArticle
   }
 }
