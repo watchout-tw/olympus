fcd66fae5f57a8292edd927a71369c5e79bbc2c0
chihaoyo <chihaoyo@gmail.com>
2019-01-28 10:52:49 +0800

    Use getWatchoutDocListURL

--- pages/read/_id.vue
+++ pages/read/_id.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="page read single">
   <div class="controls responsive-typesetting-container margin-top-bottom-single">
-    <nuxt-link :to="{ name: 'read' }" class="a-text">返回列表</nuxt-link>
+    <a :href="getWatchoutDocListURL()" class="a-text">返回列表</a>
   </div>
   <doc-header :doc="doc" :cachedAuthors="cachedAuthors" />
   <ghost-article :article="doc.content" :footnotes="footnotes" :links="links" :references="references" :data="dataOnReferences" />
@@ -10,12 +10,12 @@
 
 <script>
 import * as firestore from 'watchout-common-functions/lib/firestore'
-import { knowsCaching } from 'watchout-common-functions/interfaces'
+import { knowsCaching, knowsWatchout } from 'watchout-common-functions/interfaces'
 import { mobiledocProcessor } from 'watchout-common-functions/lib/bunko'
 import DocHeader from 'watchout-common-functions/components/comp/DocHeader'
 import GhostArticle from 'watchout-common-functions/components/ghost/Article'
 export default {
-  mixins: [knowsCaching],
+  mixins: [knowsCaching, knowsWatchout],
   async asyncData({ params }) {
     let doc = await firestore.bunko.getDoc(params.id, true)
     let mobiledoc = JSON.parse(doc.content.mobiledoc)
