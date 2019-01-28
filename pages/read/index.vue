16ae23a08aba6b5682bb0dbd6bfc546796e9ce05
chihaoyo <chihaoyo@gmail.com>
2019-01-28 20:54:22 +0800

    Add placeholder at /read

--- pages/read/index.vue
+++ pages/read/index.vue
@@ -1,5 +1,9 @@
 <template>
 <div class="page project read">
-  read
+  <div class="tcl-container margin-top-bottom-8">
+    <div class="tcl-panel tcl-left-right-margin with-top-bottom-margin">
+      A reader lives a thousand lives before he dies. The man who never reads lives only one.
+    </div>
+  </div>
 </div>
 </template>
