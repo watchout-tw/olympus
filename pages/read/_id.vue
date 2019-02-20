5123bb2582299cae0bc6142d779a42462d693708
chihaoyo <chihaoyo@gmail.com>
2019-02-20 17:22:34 +0800

    Update olympus /read/_id according to atolan

--- pages/read/_id.vue
+++ pages/read/_id.vue
@@ -1,14 +1,8 @@
 <template>
 <div class="page read single">
-  <!--<div class="controls responsive-typesetting-container margin-top-bottom-single">
-    <a :href="getWatchoutDocListURL()" class="a-text">返回列表</a>
-  </div>-->
   <doc-header :doc="doc" :cachedAuthors="cachedAuthors" />
-  <ghost-article :article="doc.content" :footnotes="footnotes" :links="links" :references="references" :data="dataOnReferences" />
-  <div class="share margin-top-bottom-double">
-    <div class="section-title with-underline text-align-center margin-top-bottom-single"><span>認同請分享</span></div>
-    <share-to-platforms :url="shareURL" />
-  </div>
+  <ghost-article :article="doc.content" :links="links" :footnotes="footnotes" :references="references" :data="dataOnReferences" />
+  <after-article :shareURL="shareURL" :links="links" :footnotes="footnotes" :references="references" :data="dataOnReferences" />
 </div>
 </template>
 
@@ -19,7 +13,7 @@ import { knowsCaching, knowsWatchout } from 'watchout-common-functions/interface
 import { mobiledocProcessor } from 'watchout-common-functions/lib/bunko'
 import DocHeader from 'watchout-common-functions/components/comp/DocHeader'
 import GhostArticle from 'watchout-common-functions/components/ghost/Article'
-import ShareToPlatforms from 'watchout-common-functions/components/ShareToPlatforms'
+import AfterArticle from 'watchout-common-functions/components/ghost/AfterArticle'
 import defaultCoverImage from 'watchout-common-assets/images/default-cover-images/musou-2-1.jpg'
 
 export default {
@@ -54,7 +48,7 @@ export default {
   components: {
     DocHeader,
     GhostArticle,
-    ShareToPlatforms
+    AfterArticle
   }
 }
 </script>
