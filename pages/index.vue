23b6c22138e00cb1f70650c7ecd29ad6308b2e73
chihaoyo <chihaoyo@gmail.com>
2019-09-27 17:42:39 +0800

    Separate list of docs & projects

--- pages/index.vue
+++ pages/index.vue
@@ -5,23 +5,49 @@
       <no-ssr><welcome /></no-ssr>
     </div>
   </div>
+  <div class="section-title with-underline small text-align-center margin-top-single margin-bottom-4"><span>最新內容</span></div>
   <div class="docs first-doc margin-top-bottom-8">
-    <reference-preview :reference="references[0]" :data="dataOnReferences" display="tcl" :show-pub-dest="true" :cachedAuthors="cachedAuthors" />
+    <reference-preview :reference="docRefs[0]" :data="dataOnReferences" display="tcl" :show-pub-dest="true" :cachedAuthors="cachedAuthors" />
   </div>
   <div class="docs tcl-container margin-top-bottom-4">
-    <div class="doc tcl-panel tcl-left-right-margin with-top-bottom-margin" :class="{ 'half-width': index >= 4 }" v-for="(reference, index) of references" :key="index" v-if="index > 0">
+    <div class="doc tcl-panel tcl-left-right-margin with-top-bottom-margin" :class="{ 'half-width': index > 2 }" v-for="(reference, index) of docRefs" :key="`doc-${index}`" v-if="index > 0">
       <reference-preview :reference="reference" :data="dataOnReferences" display="vertical" :show-pub-dest="true" :cachedAuthors="cachedAuthors" />
     </div>
     <div class="tcl-panel half-width"></div>
     <div class="tcl-panel half-width"></div>
     <div class="tcl-panel half-width"></div>
   </div>
+  <div class="actions tcl-container no-margin">
+    <div class="tcl-panel">
+      <div class="form-field-align-center">
+        <nuxt-link :to="{ name: 'docs' }" class="button medium musou">更多內容</nuxt-link>
+      </div>
+    </div>
+  </div>
+  <div class="section-title with-underline small text-align-center margin-top-single margin-bottom-4"><span>最新專題</span></div>
+  <div class="projects first-doc margin-top-bottom-8">
+    <reference-preview :reference="projectRefs[0]" :data="dataOnReferences" display="tcl" :show-pub-dest="true" :cachedAuthors="cachedAuthors" />
+  </div>
+  <div class="docs tcl-container margin-top-bottom-4">
+    <div class="doc tcl-panel tcl-left-right-margin with-top-bottom-margin" :class="{ 'half-width': index > 2 }" v-for="(reference, index) of projectRefs" :key="`doc-${index}`" v-if="index > 0">
+      <reference-preview :reference="reference" :data="dataOnReferences" display="vertical" :show-pub-dest="true" :cachedAuthors="cachedAuthors" />
+    </div>
+    <div class="tcl-panel half-width"></div>
+    <div class="tcl-panel half-width"></div>
+    <div class="tcl-panel half-width"></div>
+  </div>
+  <div class="actions tcl-container no-margin">
+    <div class="tcl-panel">
+      <div class="form-field-align-center">
+        <nuxt-link :to="{ name: 'projects' }" class="button medium musou">更多專題</nuxt-link>
+      </div>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
 import * as firestore from 'watchout-common-functions/lib/firestore'
-import * as util from 'watchout-common-functions/lib/util'
 import * as info from '~/data/info'
 import { knowsCaching, knowsWatchout } from 'watchout-common-functions/interfaces'
 import { makeReference } from 'watchout-common-functions/lib/watchout'
@@ -29,29 +55,28 @@ import Welcome from 'watchout-common-functions/components/Welcome'
 import ReferencePreview from 'watchout-common-functions/components/ReferencePreview'
 import defaultCoverImage from 'watchout-common-assets/images/default-cover-images/musou-2-1.jpg'
 
+const getLimit = 7
+
 export default {
   mixins: [knowsCaching, knowsWatchout],
   async asyncData() {
     // get docs with pub-dest filter
-    let docs = await firestore.bunko.getDocs({ pubDest: info.CHANNEL_ID })
+    let docs = await firestore.bunko.getDocs({ pubDest: info.CHANNEL_ID, limit: getLimit })
     let docRefs = docs.map(doc => makeReference('doc', doc.id, { publishedTo: doc.publishedTo }))
     let dataOnReferences = {}
     for(let i = 0; i < docRefs.length; i++) {
       dataOnReferences[docRefs[i].url] = docs[i]
     }
 
-    let projects = await firestore.bunko.getProjects({ pubDest: info.CHANNEL_ID })
+    let projects = await firestore.bunko.getProjects({ pubDest: info.CHANNEL_ID, limit: getLimit })
     let projectRefs = projects.map(project => makeReference('project', project.slug, { publishedTo: project.publishedTo }))
     for(let i = 0; i < projectRefs.length; i++) {
       dataOnReferences[projectRefs[i].url] = projects[i]
     }
 
-    let references = docRefs.concat(projectRefs)
-    references.sort((a, b) => {
-      return util.fsTSToDateObj(dataOnReferences[b.url].publishedAt) - util.fsTSToDateObj(dataOnReferences[a.url].publishedAt)
-    })
     return {
-      references,
+      docRefs,
+      projectRefs,
       dataOnReferences
     }
   },
