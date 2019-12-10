18ea6a4d4e747e58faec8818f5619e223e65d965
chihaoyo <chihaoyo@gmail.com>
2019-12-10 09:33:25 +0800

    Upgrade to nuxt 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,34 +1,58 @@
-const nodeExternals = require('webpack-node-externals')
+const pkg = require('./package')
+
 function getFavicon(projectID) {
   return 'https://raw.githubusercontent.com/watchout-tw/watchout-common-assets/master/images/logo/' + projectID + '/small.png'
 }
 function getProjectLogo(projectID) {
   return 'https://raw.githubusercontent.com/watchout-tw/watchout-common-assets/master/images/logo/' + projectID + '/large.png'
 }
+
+const info = require('./data/info')
+const siteTitle = info.SITE_TITLE
+const siteDescription = info.SITE_DESCRIPTION
+
 module.exports = {
+  mode: 'universal',
   head: {
-    title: '沃草國會無雙',
+    title: siteTitle,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'description', content: '沃草國會無雙是⋯', hid: 'description' }
+      { hid: 'description', name: 'description', content: siteDescription }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: getFavicon('musou') },
-      { rel: 'apple-touch-icon', sizes: '256x256', href: getProjectLogo('musou') }
+      { rel: 'icon', type: 'image/x-icon', href: getFavicon('watchout') },
+      { rel: 'apple-touch-icon', sizes: '256x256', href: getProjectLogo('watchout') }
     ]
   },
   loading: {
     color: '#E64C5E',
     height: '4px'
   },
+  // Global CSS
+  css: [],
+  // Plugins to load before mounting the App
+  plugins: [
+    '~/plugins/firestore.js'
+  ],
+  // Nuxt.js modules
+  modules: [
+    ['@nuxtjs/google-tag-manager', { id: 'GTM-TF76PLV' }]
+  ],
+  // Server middleware
+  serverMiddleware: [
+    '~/servermiddleware/301.js'
+  ],
+  // Build configuration
   build: {
-    vender: ['axios', 'vuex'],
-    /*
-    ** Run ESLint on save
-    */
-    extend(config, { isDev, isClient, isServer }) {
-      if(isDev && isClient) {
+    transpile: [
+      'watchout-common-assets',
+      'watchout-common-functions'
+    ],
+    // Extend webpack config here
+    extend(config, ctx) {
+      // Run ESLint on save
+      if(ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
@@ -36,23 +60,6 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
-      if(isServer) {
-        config.externals = [
-          nodeExternals({
-            whitelist: [/^watchout-common/]
-          })
-        ]
-      }
     }
-  },
-  plugins: [
-    '~/plugins/firestore.js'
-  ],
-  modules: [
-    ['@nuxtjs/google-tag-manager', { id: 'GTM-TF76PLV' }]
-  ],
-  serverMiddleware: [
-    '~/servermiddleware/301.js'
-  ]
-  // NOTE: Add Markdown loader <https://github.com/nuxt/nuxt.js/issues/1072>
+  }
 }
